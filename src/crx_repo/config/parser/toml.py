5ff5555c21512e50ea51cbc9c85135f2faf0fb2a
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-11-06 20:40:22 +0800

    Use pydantic instead deserializer

--- src/crx_repo/config/parser/toml.py
+++ src/crx_repo/config/parser/toml.py
@@ -1,9 +1,10 @@
 """ConfigParser implementation to parse toml config."""
 
 import tomllib
+from typing import Any
 from typing import override
 from pathlib import Path
-from deserializer import deserialize
+from pydantic import TypeAdapter
 from crx_repo.config.config import Config
 from crx_repo.config.parser.parser import PathOrStr
 from crx_repo.config.parser.parser import ConfigParser
@@ -18,10 +19,11 @@ class TomlConfigParser(ConfigParser):
         if path not in self._cache:
             content = path.read_text()
             config_raw = tomllib.loads(content)
-            self._cache[path] = deserialize(
-                Config, config_raw,
-                lambda x: x.replace("-", "_").lower(),  # Kebab case to snake case
-            )
+            t = TypeAdapter(Config)
+            converted_config: dict[str, Any] = {}
+            for key in config_raw:
+                converted_config[key.replace("-", "_").lower()] = config_raw[key]
+            self._cache[path] = t.validate_python(converted_config)
         return self._cache[path]
 
     @override
