2c29a10eaa6721db253411da4470168ece6cebca
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2025-06-15 12:26:36 +0800

    Use typer to parse cli arguments
    We can generate shell completion with it.

--- src/crx_repo/__init__.py
+++ src/crx_repo/__init__.py
@@ -1,23 +1,26 @@
 """Download Chrom(e|ium) extensions from Chrome Web Store and serve a update manifest."""
 
-import sys
 import logging
-from typing import Literal
+from typer import Exit
+from typer import Typer
+from typer import Option
+from typing import Annotated
 from aiohttp import web
 from pathlib import Path
-from argparse import Namespace
-from argparse import ArgumentParser
 
 
 try:
     from uvloop import run as _run_async
+
     uvloop = True
 except ImportError:
     from asyncio import run as _run_async
+
     uvloop = False
 
 from crx_repo.server import run_app as _run_app
 from crx_repo.server import setup_server as _setup_server
+from crx_repo.config.config import LogLevelType
 from crx_repo.config.parser import parse_config_async as _parse_config_async
 
 
@@ -25,10 +28,14 @@ __version__ = "0.2.1"
 
 
 _logger = logging.getLogger(__name__)
+main = Typer(help=__doc__)
 
 
 def _setup_logger(logger: logging.Logger = _logger):
-    fmt = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s", "%Y-%m-%d %H:%M:%S")
+    fmt = logging.Formatter(
+        "%(asctime)s-%(levelname)s-%(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
     if len(logger.handlers) == 0:
         logger.addHandler(logging.StreamHandler())
     for handler in logger.handlers:
@@ -36,33 +43,7 @@ def _setup_logger(logger: logging.Logger = _logger):
         handler.setFormatter(fmt)
 
 
-def _parse_args() -> Namespace:
-    parser = ArgumentParser(
-        description=sys.modules[__name__].__doc__
-    )
-    _ = parser.add_argument(
-        "-v", "--version",
-        help="Print program version.",
-        action="version",
-        version=__version__,
-    )
-    _ = parser.add_argument(
-        "-c", "--config",
-        help="Config file path.",
-    )
-    _ = parser.add_argument(
-        "-l", "--log-level",
-        help="Log level before config is load.",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        default="INFO"
-    )
-    return parser.parse_args()
-
-
-async def _main_async(
-    config_path: str,
-    log_level_in_arg: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-):
+async def _main_async(config_path: str, log_level_in_arg: LogLevelType):
     config = await _parse_config_async(config_path)
     if log_level_in_arg != config.log_level:
         _logger.setLevel(config.log_level)
@@ -70,10 +51,7 @@ async def _main_async(
     if config.listen.tcp is None and config.listen.unix is None:
         _logger.critical("No valid listen config found.")
         raise RuntimeError
-    app = _setup_server(
-        config,
-        _logger.level == logging.DEBUG
-    )
+    app = _setup_server(config, _logger.level == logging.DEBUG)
     tcp = config.listen.tcp
     host = tcp.address if tcp is not None else None
     port = tcp.port if tcp is not None else None
@@ -90,22 +68,39 @@ async def _main_async(
             _logger.debug("Setting permission of socket to %s", permission)
             Path(path).chmod(permission)
             yield
+
         app.cleanup_ctx.append(set_permission)
 
     await _run_app(app, (host, port, tls_tcp), (path, tls_unix))
 
 
-def main():
-    """Main entrance of cli."""
-    args = _parse_args()
-    # Temp value, may be overriden by config.
-    _logger.setLevel(args.log_level)  # pyright: ignore[reportAny]
+def __version(value: bool):
+    if value:
+        print(__version__)  # noqa: T201
+        raise Exit
+
+
+@main.command()
+def __main(  # pyright: ignore[reportUnusedFunction]
+    config: Annotated[Path, Option(help="Config file path.")],
+    log_level: Annotated[
+        LogLevelType,
+        Option(
+            help="Log level before config is load.",
+            parser=str,
+        ),
+    ] = "INFO",
+    _: Annotated[
+        bool,
+        Option(
+            "--version",
+            help="Print program version.",
+            callback=__version,
+        ),
+    ] = False,
+):
+    _logger.setLevel(log_level)
     _setup_logger()
     if uvloop:
         _logger.info("Using uvloop as async event loop.")
-    _run_async(
-        _main_async(
-            args.config,  # pyright: ignore[reportAny]
-            args.log_level  # pyright: ignore[reportAny]
-        )
-    )
+    _run_async(_main_async(str(config), log_level))
