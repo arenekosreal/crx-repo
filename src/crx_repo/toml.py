3410416fd794112ef8b280c6a9c8a7182ca31b70
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2025-06-17 15:49:14 +0800

    Return None when config file is not an valid toml document

--- src/crx_repo/toml.py
+++ src/crx_repo/toml.py
@@ -4,6 +4,7 @@ from typing import override
 from .config import Config
 from .config import ConfigParser
 from pathlib import Path
+from tomllib import TOMLDecodeError
 from tomllib import loads
 from aiofiles import open as aioopen
 from pydantic import ValidationError
@@ -17,10 +18,10 @@ class TomlConfigParser(ConfigParser):
         if not config.exists():
             return None
         async with aioopen(config, "r") as reader:
-            config_dict = loads(await reader.read())
             try:
+                config_dict = loads(await reader.read())
                 config_object = Config.model_validate(config_dict, by_alias=True)
-            except ValidationError:
+            except (TOMLDecodeError, ValidationError):
                 config_object = None
 
             return config_object
